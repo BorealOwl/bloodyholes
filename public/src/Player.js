6a2e9bcd9a6c66b3b65c41e2f13195063a39d282
BorealOwl <cbv9000000@gmail.com>
2015-06-17 23:53:10 +0300

    Copied from sourse

--- public/src/Player.js
+++ public/src/Player.js
@@ -1,20 +1,75 @@
-require([], function () { 
+require([], function () {
+  Q.Sprite.extend('Actor', {
+    init: function (p) {
+      this._super(p, {
+        update: true
+      });
+
+      var temp = this;
+      setInterval(function () {
+        if (!temp.p.update) {
+          temp.destroy();
+        }
+        temp.p.update = false;
+      }, 3000);
+    } 
+  });
+
   Q.Sprite.extend('Player', {
     init: function (p) {
       this._super(p, {
-        sheet: 'player'
+        sheet: 'player',
+        tagged: false,
+        invincible: false,
+        vyMult: 1
       });
- 
       this.add('2d, platformerControls, animation');
+
+      this.addEventListeners();
+    },
+    addEventListeners: function () {
+      this.on('hit', function (collision) {
+        if (this.p.tagged && collision.obj.isA('Actor') && !collision.obj.p.tagged && !collision.obj.p.invincible) {
+          this.p.socket.emit('tag', { playerId: collision.obj.p.playerId });
+          this.p.tagged = false;
+          this.p.sheet = 'player';
+          this.p.invincible = true;
+          this.p.opacity = 0.5;
+          this.p.speed = 300;
+          this.p.vyMult = 1.5;
+          var temp = this;
+          setTimeout(function () {
+            temp.p.invincible = false;
+            temp.p.opacity = 1;
+            temp.p.speed = 200;
+            temp.p.vyMult = 1;
+          }, 3000);
+        }
+      });
+
+      this.on('join', function () {
+        this.p.invincible = true;
+        this.p.opacity = 0.5;
+        this.p.speed = 300;
+        this.p.vyMult = 1.5;
+        var temp = this;
+        setTimeout(function () {
+          temp.p.invincible = false;
+          temp.p.opacity = 1;
+          temp.p.speed = 200;
+          temp.p.vyMult = 1;
+        }, 3000);
+      });
     },
     step: function (dt) {
       if (Q.inputs['up']) {
-        this.p.vy = -200;
+        this.p.vy = -200 * this.p.vyMult;
       } else if (Q.inputs['down']) {
-        this.p.vy = 200;
+        this.p.vy = 200 * this.p.vyMult;
       } else if (!Q.inputs['down'] && !Q.inputs['up']) {
         this.p.vy = 0;
       }
+      this.p.socket.emit('update', { playerId: this.p.playerId, x: this.p.x, y: this.p.y, sheet: this.p.sheet, opacity: this.p.opacity, invincible: this.p.invincible, tagged: this.p.tagged });
     }
   });
 });
--- public/src/game.js
+++ public/src/game.js
@@ -3,28 +3,88 @@ var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
       .setup({ maximize: true })
       .enableSound()
       .controls().touch();
- 
+
 Q.gravityY = 0;
- 
+
+require(['socket.io/socket.io.js']);
+
+var players = [];
+var socket = io.connect('http://104.167.115.218:8080');
+var UiPlayers = document.getElementById("players");
+var selfId, player;
+
 var objectFiles = [
   './src/player'
 ];
- 
+
 require(objectFiles, function () {
+  function setUp (stage) {
+    socket.on('count', function (data) {
+      UiPlayers.innerHTML = 'Players: ' + data['playerCount'];
+    });
+
+    socket.on('connected', function (data) {
+      selfId = data['playerId'];
+      if (data['tagged']) {
+        player = new Q.Player({ playerId: selfId, x: 100, y: 100, socket: socket });
+        player.p.sheet = 'enemy'
+        player.p.tagged = true;
+        stage.insert(player);
+      } else {
+        player = new Q.Player({ playerId: selfId, x: 100, y: 100, socket: socket });
+        stage.insert(player);
+        player.trigger('join');
+      }
+      stage.add('viewport').follow(player);
+    });
+
+    socket.on('updated', function (data) {
+      var actor = players.filter(function (obj) {
+        return obj.playerId == data['playerId'];
+      })[0];
+      if (actor) {
+        actor.player.p.x = data['x'];
+        actor.player.p.y = data['y'];
+        actor.player.p.sheet = data['sheet'];
+        actor.player.p.opacity = data['opacity'];
+        actor.player.p.invincible = data['invincible'];
+        actor.player.p.tagged = data['tagged'];
+        actor.player.p.update = true;
+      } else {
+        var temp = new Q.Actor({ playerId: data['playerId'], x: data['x'], y: data['y'], sheet: data['sheet'], opacity: data['opacity'], invincible: data['invincible'], tagged: data['tagged'] });
+        players.push({ player: temp, playerId: data['playerId'] });
+        stage.insert(temp);
+      }
+    });
+
+    socket.on('tagged', function (data) {
+      if (data['playerId'] == selfId) {
+        player.p.sheet = 'enemy';
+        player.p.tagged = true;
+      } else {
+        var actor = players.filter(function (obj) {
+          return obj.playerId == data['playerId'];
+        })[0];
+        if (actor) {
+          actor.player.p.sheet = 'enemy'
+        }
+      }
+    });
+  }
+
   Q.scene('arena', function (stage) {
     stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
- 
-    var player = stage.insert(new Q.Player({ x: 100, y: 100 }));
-    stage.add('viewport').follow(player);
+
+    setUp(stage);
   });
- 
+
   var files = [
     '/images/tiles.png',
     '/maps/arena.json',
     '/images/sprites.png',
     '/images/sprites.json'
   ];
- 
+
   Q.load(files.join(','), function () {
     Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
     Q.compileSheets('/images/sprites.png', '/images/sprites.json');
